59ad62bd5dc537a6a45f60e0e88e6e28fc539226
Isabelle Santin <isabelle@chosenfewsoftware.com>
2024-10-15 10:48:31 -0500

    Add project files.

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace SubverseIM.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace SubverseIM.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
