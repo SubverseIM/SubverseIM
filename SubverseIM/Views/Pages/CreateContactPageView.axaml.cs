70bee2f2363d5ceefd79263d8333da351baa243c
Isabelle Santin <isabelle@chosenfewsoftware.com>
2024-12-13 14:51:58 -0600

    Add accessible text editing to CreateContactPage

--- SubverseIM/Views/Pages/CreateContactPageView.axaml.cs
+++ SubverseIM/Views/Pages/CreateContactPageView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using SubverseIM.Services;
+using SubverseIM.ViewModels.Pages;
 
 namespace SubverseIM.Views.Pages;
 
@@ -7,5 +9,26 @@ public partial class CreateContactPageView : UserControl
     public CreateContactPageView()
     {
         InitializeComponent();
+
+        nameEditBox.GotFocus += TextBoxGotFocus;
+        noteEditBox.GotFocus += TextBoxGotFocus;
+    }
+
+    private async void TextBoxGotFocus(object? sender, Avalonia.Input.GotFocusEventArgs e)
+    {
+        ILauncherService launcherService = await (DataContext as CreateContactPageViewModel)!
+            .ServiceManager.GetWithAwaitAsync<ILauncherService>();
+
+        if (launcherService.IsAccessibilityEnabled && sender is TextBox textBox)
+        {
+            textBox.IsEnabled = false;
+
+            string? messageText = await launcherService.ShowInputDialogAsync(
+                textBox.Watermark ?? "Enter Input Text", textBox.Text
+                );
+            textBox.Text = messageText;
+
+            textBox.IsEnabled = true;
+        }
     }
 }
