f6d6dad41465182916561f00d3cc510b98ac59f2
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-01-28 10:06:12 -0600

    Begin writing UI tests

---
+++ SubverseIM.App/SubverseIM.Headless/Main.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Headless;
+using SubverseIM.Tests;
+
+[assembly: AvaloniaTestApplication(typeof(TestAppBuilder))]
+namespace SubverseIM.Tests
+{
+    public class TestAppBuilder
+    {
+        public static AppBuilder BuildAvaloniaApp() => AppBuilder
+            .Configure<App>(() => new App(,))
+            .UseHeadless(new AvaloniaHeadlessPlatformOptions());
+    }
+}
