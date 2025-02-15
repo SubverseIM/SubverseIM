a00903dc9b6dcb6e4581584e97870fd4911477fc
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-02-15 15:26:44 -0600

    Add image embeds for chat

--- SubverseIM.App/SubverseIM/ViewModels/Components/EmbedViewModel.cs
+++ SubverseIM.App/SubverseIM/ViewModels/Components/EmbedViewModel.cs
@@ -1,19 +1,74 @@
-﻿using MonoTorrent;
+﻿using Avalonia.Media.Imaging;
+using MonoTorrent;
+using OpenGraphNet;
+using SubverseIM.Models;
+using SubverseIM.Services;
 using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace SubverseIM.ViewModels.Components
 {
     public class EmbedViewModel : ViewModelBase
     {
-        public string AbsoluteUri { get; }
+        private readonly IServiceManager serviceManager;
 
-        public string Name => MagnetLink.TryParse(AbsoluteUri,
-            out MagnetLink? magnetLink) ? magnetLink.Name ?? "Untitled" :
-            new Uri(AbsoluteUri).Host;
+        public Uri AbsoluteUri { get; }
 
-        public EmbedViewModel(string uriString)
+        public string DisplayName => MagnetLink.TryParse(
+            AbsoluteUri.OriginalString, out MagnetLink? magnetLink) ?
+            magnetLink.Name ?? "Untitled" :
+            AbsoluteUri.Host;
+
+        public Task<Bitmap?> FetchedBitmap { get; }
+
+        public EmbedViewModel(IServiceManager serviceManager, string uriString)
         {
-            AbsoluteUri = uriString;
+            this.serviceManager = serviceManager;
+            AbsoluteUri = new Uri(uriString);
+            FetchedBitmap = GetBitmapAsync();
+        }
+
+        public async Task<Bitmap?> GetBitmapAsync()
+        {
+            OpenGraph og;
+            ITorrentService torrentService = await serviceManager.GetWithAwaitAsync<ITorrentService>();
+            Progress<TorrentStatus>? progress = await torrentService.StartAsync(new SubverseTorrent(AbsoluteUri.OriginalString));
+            if (progress is not null)
+            {
+                TaskCompletionSource tcs = new();
+                progress.ProgressChanged += (s, ev) =>
+                {
+                    if (ev.Complete) tcs.TrySetResult();
+                };
+                await tcs.Task;
+
+                string cacheDirPath = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "torrent", "files"
+                        );
+                string cacheFilePath = Path.Combine(cacheDirPath,
+                    DisplayName ?? throw new InvalidOperationException("No display name was provided for this file!")
+                    );
+
+                using Stream stream = File.OpenRead(cacheFilePath);
+                return Bitmap.DecodeToWidth(stream, 256);
+            }
+            else if ((og = await OpenGraph.ParseUrlAsync(AbsoluteUri)).Image is not null)
+            {
+                using MemoryStream bufferStream = new();
+                using (HttpClient http = new())
+                using (Stream downloadStream = await http.GetStreamAsync($"{AbsoluteUri.GetLeftPart(UriPartial.Authority)}{og.Image.AbsolutePath}"))
+                {
+                    await downloadStream.CopyToAsync(bufferStream);
+                }
+                bufferStream.Position = 0;
+                return Bitmap.DecodeToWidth(bufferStream, 256);
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
