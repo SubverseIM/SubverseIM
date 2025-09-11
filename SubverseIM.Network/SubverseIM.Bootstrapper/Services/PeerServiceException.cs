9cba27a1a0c8927f14c88d949fd98aa259ac492b
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-09-11 13:25:54 -0500

    Feature: WebSocket message relay

---
+++ SubverseIM.Network/SubverseIM.Bootstrapper/Services/PeerServiceException.cs
@@ -0,0 +1,19 @@
+﻿
+namespace SubverseIM.Bootstrapper.Services
+{
+    [Serializable]
+    internal class PeerServiceException : Exception
+    {
+        public PeerServiceException()
+        {
+        }
+
+        public PeerServiceException(string? message) : base(message)
+        {
+        }
+
+        public PeerServiceException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
