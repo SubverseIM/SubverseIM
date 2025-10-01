08a2bcb0dd71a163d7206f2e7154bd3bdeefc04c
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-10-01 09:46:38 -0500

    Fix: notification payloads on iOS should include `UserInfo`

---
+++ SubverseIM.Network/SubverseIM.Bootstrapper/Models/CustomNotificationContainer.cs
@@ -0,0 +1,20 @@
+﻿using Fitomad.Apns.Entities.Notification;
+using System.Text.Json.Serialization;
+
+namespace SubverseIM.Bootstrapper.Models
+{
+    public class CustomNotificationContainer : NotificationContainer 
+    {
+        [JsonPropertyName("com.ChosenFewSoftware.SubverseIM.BodyContent")]
+        public string? BodyContent { get; init; }
+
+        [JsonPropertyName("com.ChosenFewSoftware.SubverseIM.ConversationParticipants")]
+        public string? ConversationParticipants { get; init; }
+
+        [JsonPropertyName("com.ChosenFewSoftware.SubverseIM.MessageTopic")]
+        public string? MessageTopic { get; init; }
+
+        [JsonPropertyName("com.ChosenFewSoftware.SubverseIM.SenderId")]
+        public string? SenderId { get; init; }
+    }
+}
