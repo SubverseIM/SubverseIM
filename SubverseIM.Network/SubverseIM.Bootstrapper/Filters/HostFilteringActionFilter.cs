4f3fe3bcee69b6d5526f41366ccd47194e8c4ee4
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-11-04 12:32:49 -0600

    Fix: only allow calls to `/blob/expire-all` on `localhost`

---
+++ SubverseIM.Network/SubverseIM.Bootstrapper/Filters/HostFilteringActionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SubverseIM.Bootstrapper.Filters
+{
+    public class HostFilteringActionFilterAttribute : ActionFilterAttribute
+    {
+        private readonly string[] _allowedHosts;
+
+        public HostFilteringActionFilterAttribute(string[] allowedHosts)
+        {
+            _allowedHosts = allowedHosts;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var host = context.HttpContext.Request.Host.Host;
+            if (!_allowedHosts.Contains(host, StringComparer.OrdinalIgnoreCase))
+            {
+                context.Result = new UnauthorizedResult(); // Or a custom forbidden result
+            }
+        }
+    }
+}
