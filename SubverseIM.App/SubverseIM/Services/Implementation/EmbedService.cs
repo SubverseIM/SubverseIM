ed7f5a551ff2bf8bf6c9dc4d9ec213b99d124920
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-11-14 11:05:28 -0600

    Feature: use application-wide cache service for embedded images

---
+++ SubverseIM.App/SubverseIM/Services/Implementation/EmbedService.cs
@@ -0,0 +1,83 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SubverseIM.Services.Implementation
+{
+    public class EmbedService : IEmbedService
+    {
+        private readonly IServiceManager serviceManager;
+
+        private readonly ConcurrentDictionary<string, TaskCompletionSource<Image>> cachedImageMap;
+
+        public EmbedService(IServiceManager serviceManager)
+        {
+            this.serviceManager = serviceManager;
+            cachedImageMap = new();
+        }
+
+        public async Task<Image> GetCacheImageAsync(FileInfo imageFileInfo, CancellationToken cancellationToken)
+        {
+            string imageFilePath = imageFileInfo.FullName;
+
+            bool addedFlag = false;
+            TaskCompletionSource<Image> imageTcs = cachedImageMap.GetOrAdd(
+                imageFilePath, x => { addedFlag = true; return new(); });
+
+            if (addedFlag)
+            {
+                try
+                {
+                    using Stream imageFileStream = imageFileInfo.OpenRead();
+                    Image image = await Image.LoadAsync<Bgra32>(imageFileStream, cancellationToken);
+                    imageTcs.SetResult(image);
+                }
+                catch (OperationCanceledException)
+                {
+                    imageTcs.SetCanceled(cancellationToken);
+                }
+                catch (Exception ex) 
+                {
+                    imageTcs.SetException(ex);
+                }
+            }
+
+            return await imageTcs.Task;
+        }
+
+        public async Task<Image> GetCacheImageAsync(Uri imageUri, CancellationToken cancellationToken)
+        {
+            string imageUriStr = imageUri.OriginalString;
+
+            bool addedFlag = false;
+            TaskCompletionSource<Image> imageTcs = cachedImageMap.GetOrAdd(
+                imageUriStr, x => { addedFlag = true; return new(); });
+
+            if (addedFlag)
+            {
+                try
+                {
+                    HttpClient httpClient = await serviceManager.GetWithAwaitAsync<HttpClient>();
+                    using Stream imageUriStream = await httpClient.GetStreamAsync(imageUri, cancellationToken);
+                    Image image = await Image.LoadAsync<Bgra32>(imageUriStream, cancellationToken);
+                    imageTcs.SetResult(image);
+                }
+                catch (OperationCanceledException)
+                {
+                    imageTcs.SetCanceled(cancellationToken);
+                }
+                catch (Exception ex)
+                {
+                    imageTcs.SetException(ex);
+                }
+            }
+
+            return await imageTcs.Task;
+        }
+    }
+}
