122e715024f51dbc2bec897d5811d826d448e8dc
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-11-15 10:32:58 -0600

    Feature: network awareness API for bootstrappers

--- SubverseIM.Network/SubverseIM.Bootstrapper/Services/DbService.cs
+++ SubverseIM.Network/SubverseIM.Bootstrapper/Services/DbService.cs
@@ -12,15 +12,21 @@ namespace SubverseIM.Bootstrapper.Services
 
         private bool _disposedValue;
 
-        public DbService(IConfiguration configuration) 
+        public DbService(IConfiguration configuration)
         {
             _configuration = configuration;
 
             BsonMapper mapper = new();
+
             mapper.RegisterType(
                 serialize: (peerId) => peerId.ToString(),
                 deserialize: (bson) => SubversePeerId.FromString(bson.AsString)
             );
+
+            mapper.RegisterType(
+                serialize: (uri) => uri.OriginalString,
+                deserialize: (bson) => new Uri(bson.AsString)
+                );
 
             _dbContext = new LiteDatabase(_configuration.GetConnectionString("LiteDb"), mapper);
         }
@@ -34,9 +40,48 @@ namespace SubverseIM.Bootstrapper.Services
                 messages.Insert(message);
                 return true;
             }
-            catch (LiteException ex) when (ex.ErrorCode == LiteException.INDEX_DUPLICATE_KEY) 
+            catch (LiteException ex) when (ex.ErrorCode == LiteException.INDEX_DUPLICATE_KEY)
             {
                 return false;
+            }
+        }
+
+        public bool InsertFriend(SubverseFriend friend)
+        {
+            var friends = _dbContext.GetCollection<SubverseFriend>();
+            friends.EnsureIndex(x => x.Address, unique: true);
+
+            SubverseFriend? oldFriend = friends.FindOne(x => x.Address == friend.Address);
+            friend.Id = oldFriend.Id;
+
+            return friends.Upsert(friend);
+        }
+
+        public IEnumerable<SubverseFriend> GetRecentFriends(int? maxListCount, TimeSpan? expireTime)
+        {
+            var now = DateTime.UtcNow;
+            var friends = _dbContext.GetCollection<SubverseFriend>();
+
+            ILiteQueryable<SubverseFriend> query;
+            if (expireTime.HasValue)
+            {
+                query = friends.Query()
+                    .Where(x => now - x.LastSeenOn < expireTime)
+                    .OrderByDescending(x => x.LastSeenOn);
+            }
+            else
+            {
+                query = friends.Query()
+                    .OrderByDescending(x => x.LastSeenOn);
+            }
+
+            if (maxListCount.HasValue)
+            {
+                return query.Limit(maxListCount.Value).ToEnumerable();
+            }
+            else
+            {
+                return query.ToEnumerable();
             }
         }
 
